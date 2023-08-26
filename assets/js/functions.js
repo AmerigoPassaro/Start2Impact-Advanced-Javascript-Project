8e9c5cf1283519273717566b10676a1bc029e6ef
AmerigoPassaro <110347819+AmerigoPassaro@users.noreply.github.com>
2023-08-26 15:32:14 +0200

    Sostituzione File
    Sostituzione file Index e cartella Assets e rispettivo contenuto, per avanzamento lavori

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,20 +1,62 @@
-let table = document.querySelector('#seach-result');
-let description = document.querySelector('#book-description');
+var limit = 0;
+let bodyTable = document.querySelector("#results");
+let boxDes = document.querySelector("#book-description");
 
-function openTab(){
-  if(table.style.display === "none"){
-    table.style.display = "table";
-    description.style.display = "none";
-  } else {
-    table.style.display = "none";
-  }
-}
+//Funzione di apertura descrizione {COMPLETA}
 
 function openDes(){
-  if(description.style.display === "none"){
-    description.style.display = "block";
-        table.style.display = "none";
-  } else {
-    description.style.display = "none";
+  //Estrapolazione della chiave del libro
+  let keyBook = event.target.parentNode.parentNode.title;
+  // Collegamento al Json
+  fetch(`https://openlibrary.org/works/${keyBook}.json`)
+  .then(response => response.json())
+  .then(commits => (
+    boxDes.insertAdjacentHTML("beforeend",
+    // Inserimento descrizione
+    `<h2>${commits.title}</h2>
+    <p>${commits.description}</p>
+    <button id="search-button" onclick="closeDe()">Close</button>`)))
+  //Cancellazione della tabella
+  bodyTable.innerHTML = "";
+  bodyTable.parentNode.style.display = "none";
+  i= -12;
+}
+
+// Funzione di chiusura descrizione {COMPLETA}
+
+function closeDe(){
+  //Cancellazione della descrizione
+  boxDes.innerHTML = "";
+  //Azzeramento contatori
+  i= -12;
+}
+
+//Funzione di apertura report di ricerca
+
+function search(){
+  //Azzeramento contatori se la tabella è vuota
+  if(bodyTable.innerHTML === ""){i= -12};
+  // Eliminazione descrizione
+  boxDes.innerHTML = "";
+  // Etrapolazione dati dall'input
+  let myGenre = document.querySelector("#search-input").value.toLowerCase();
+  // Ciclo di creazione riga
+  for (var i = -12; myGenre !== "" && limit > i; i++){
+    // Visualizzazione tabella
+    bodyTable.parentNode.style.display = "table";
+    // Collegamento al Json
+    fetch(`https://openlibrary.org/subjects/${myGenre}.json`)
+    .then(response => response.json())
+    .then(commits => (
+      // Estrapolazione limite ed chiave
+      mainKey = commits.works[i].key,
+      limit = commits.works.length,
+      // Inserimento riga
+      bodyTable.insertAdjacentHTML("beforeend",
+      `<tr id="${i}" class="result-item" title="${mainKey.slice(7, mainKey.length)}">
+      <td>${i+1}</td>
+      <td><button class="result-title" onclick="openDes()">${commits.works[i].title}</button></td>
+      <td>${commits.works[i].authors[0].name}</td>
+      </tr>`), i++))
   }
 }
