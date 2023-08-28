58e88e092b13030d3f09020f4b9f066a942fdb2d
AmerigoPassaro <110347819+AmerigoPassaro@users.noreply.github.com>
2023-08-28 17:00:33 +0200

    Sostituzione File
    Sostituzione file per avanzamento dei lavori
    
    HTML
    CSS
    JavaScript
    Immagini

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,4 +1,5 @@
 var limit = 0;
+var i = 0;
 let bodyTable = document.querySelector("#results");
 let boxDes = document.querySelector("#book-description");
 
@@ -19,7 +20,7 @@ function openDes(){
   //Cancellazione della tabella
   bodyTable.innerHTML = "";
   bodyTable.parentNode.style.display = "none";
-  i= -12;
+  i = 0;
 }
 
 // Funzione di chiusura descrizione {COMPLETA}
@@ -27,36 +28,36 @@ function openDes(){
 function closeDe(){
   //Cancellazione della descrizione
   boxDes.innerHTML = "";
-  //Azzeramento contatori
-  i= -12;
 }
 
 //Funzione di apertura report di ricerca
 
 function search(){
-  //Azzeramento contatori se la tabella è vuota
-  if(bodyTable.innerHTML === ""){i= -12};
   // Eliminazione descrizione
   boxDes.innerHTML = "";
+  bodyTable.innerHTML = "";
   // Etrapolazione dati dall'input
   let myGenre = document.querySelector("#search-input").value.toLowerCase();
   // Ciclo di creazione riga
-  for (var i = -12; myGenre !== "" && limit > i; i++){
-    // Visualizzazione tabella
-    bodyTable.parentNode.style.display = "table";
-    // Collegamento al Json
-    fetch(`https://openlibrary.org/subjects/${myGenre}.json`)
-    .then(response => response.json())
-    .then(commits => (
-      // Estrapolazione limite ed chiave
-      mainKey = commits.works[i].key,
-      limit = commits.works.length,
-      // Inserimento riga
-      bodyTable.insertAdjacentHTML("beforeend",
-      `<tr id="${i}" class="result-item" title="${mainKey.slice(7, mainKey.length)}">
-      <td>${i+1}</td>
-      <td><button class="result-title" onclick="openDes()">${commits.works[i].title}</button></td>
-      <td>${commits.works[i].authors[0].name}</td>
-      </tr>`), i++))
+  if(myGenre !== ""){
+    for (i = -12; limit > i; i++){
+      // Visualizzazione tabella
+      bodyTable.parentNode.style.display = "table";
+      // Collegamento al Json
+      fetch(`https://openlibrary.org/subjects/${myGenre}.json`)
+      .then(response => response.json())
+      .then(commits => (
+        // Estrapolazione limite ed chiave
+        mainKey = commits.works[i].key,
+        limit = commits.works.length,
+        // Inserimento riga
+        bodyTable.insertAdjacentHTML("beforeend",
+        `<tr id="${i}" class="result-item" title="${mainKey.slice(7, mainKey.length)}">
+        <td>${i+1}</td>
+        <td><button class="result-title" onclick="openDes()">${commits.works[i].title}</button></td>
+        <td>${commits.works[i].authors[0].name}</td>
+        </tr>`), i++))
+    }
   }
+  i = 0;
 }
